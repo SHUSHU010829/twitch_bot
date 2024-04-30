985f3d23102663331337da6dfbf4aa506a67cd29
SHUSHU010829 <shushu90829@gmail.com>
2024-04-30 13:55:34 +0800

    fix: 點歌api 修正

--- src/commands.js
+++ src/commands.js
@@ -1,57 +1,61 @@
 const commands = {
-    '!指令': '目前指令：!樂透 !骰 !運勢 !dc !ffz !bgm，剩下自己猜~',
-    '!樂透': getLottoNumbers,
-    '!骰': getDice,
-    '!運勢': getFortune,
-    '!點歌': getSong,
-    '!請問': '問就是下次一定！',
-    '!bgm':'Background Music Made by Hunter Milo. > https://youtu.be/ewywwZe7cDI?si=HP1qpQicjPB2djN_',
-    '!開台': '幾乎天天都會小開一下，平日時段看我下班時間，假日大概都是中午開台除非我睡死 # 佛系開台！如果有請假會在我的 DC 群告知 ~',
-    '!ffz':'在線上應用程式插件下載 FrankerFaceZ：https://chrome.google.com/webstore/detail/frankerfacez/fadndhdgpmmaapbmfcknlfgcflmmmieb?hl=zh-TW，進 FFZ 設定 Add-Ons 把 7tv 打開就能看到啦！',
-    '?': 'shushu23What',
-    '老': '我才不老，我三歲！ FrogeMad',
-    '早安': '早安！晚安！友露安！ Evilowo',
-    'ㄤㄤ': 'ㄤ什麼ㄤ！好好打字！ mindyouFroge',
-    '副歌': '我就是副歌超人！ 其它都不會 OkaygeClap',
-    '嗆': '哪有倫家最溫柔了~~~~ shushu23Love',
-    '派': '哪有倫家最溫柔了~~~~ shushu23Love',
-    '安安': sayHello,
+  "!指令": "目前指令：!樂透 !骰 !運勢 !dc !ffz !bgm，剩下自己猜~",
+  "!樂透": getLottoNumbers,
+  "!骰": getDice,
+  "!運勢": getFortune,
+  "!點歌": getSong,
+  "!請問": "問就是下次一定！",
+  "!bgm":
+    "Background Music Made by Hunter Milo. > https://youtu.be/ewywwZe7cDI?si=HP1qpQicjPB2djN_",
+  "!開台":
+    "幾乎天天都會小開一下，平日時段看我下班時間，假日大概都是中午開台除非我睡死 # 佛系開台！如果有請假會在我的 DC 群告知 ~",
+  "!ffz":
+    "在線上應用程式插件下載 FrankerFaceZ：https://chrome.google.com/webstore/detail/frankerfacez/fadndhdgpmmaapbmfcknlfgcflmmmieb?hl=zh-TW，進 FFZ 設定 Add-Ons 把 7tv 打開就能看到啦！",
+  "?": "shushu23What",
+  老: "我才不老，我三歲！ FrogeMad",
+  早安: "早安！晚安！友露安！ Evilowo",
+  ㄤㄤ: "ㄤ什麼ㄤ！好好打字！ mindyouFroge",
+  副歌: "我就是副歌超人！ 其它都不會 OkaygeClap",
+  嗆: "哪有倫家最溫柔了~~~~ shushu23Love",
+  派: "哪有倫家最溫柔了~~~~ shushu23Love",
+  安安: sayHello,
 };
 
 function sayHello() {
-    return "噗噗安安 shushu23Hi";
+  return "噗噗安安 shushu23Hi";
 }
 
 function getLottoNumbers() {
-    const numbers = new Set();
-    while (numbers.size < 6) {
-        numbers.add(Math.floor(Math.random() * 49) + 1);
-    }
-    const sortedNumbers = [...numbers].sort((a, b) => a - b);
-    return `樂透預測：${sortedNumbers.join(', ')}，祝您中大獎！🎰`;
+  const numbers = new Set();
+  while (numbers.size < 6) {
+    numbers.add(Math.floor(Math.random() * 49) + 1);
+  }
+  const sortedNumbers = [...numbers].sort((a, b) => a - b);
+  return `樂透預測：${sortedNumbers.join(", ")}，祝您中大獎！🎰`;
 }
 
 function getDice() {
-    const dice = Math.floor(Math.random() * 6) + 1;
-    return `骰到${dice}點`;
+  const dice = Math.floor(Math.random() * 6) + 1;
+  return `骰到${dice}點`;
 }
 
 function getFortune() {
-    const strawList = [
-            "🌈 大吉",
-            "🔆 中吉",
-            "✨ 小吉",
-            "💤 平淡無奇",
-            "💥 凶",
-            "🔥 大凶",
-        ];
-    const result = strawList[Math.floor(Math.random() * strawList.length)];
-    return `今日運勢：${result}`;
+  const strawList = [
+    "🌈 大吉",
+    "🔆 中吉",
+    "✨ 小吉",
+    "💤 平淡無奇",
+    "💥 凶",
+    "🔥 大凶",
+  ];
+  const result = strawList[Math.floor(Math.random() * strawList.length)];
+  return `今日運勢：${result}`;
 }
 
 async function getSong(songName) {
-    console.log('點歌指令：' + songName);
-    return `點歌指令已接收：${songName}`;
+  console.log("點歌指令：" + songName);
+  //   return `點歌指令已接收：${songName}`;
+  return "下次一定！";
 }
 
 module.exports = commands;
